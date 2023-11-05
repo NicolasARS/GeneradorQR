1a316b8d8b7917239beabc6306ee4b7aa8274731
NicolasARS <aluriv9412@ieselcaminas.org>
2023-11-05 23:40:11 +0100

    Primeros pasos hasta formulario contacto

---
+++ src/Controller/PageController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
+use App\Entity\Contact;
+use Symfony\Component\HttpFoundation\Request;
+use App\Form\ContactFormType;
+
+class PageController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function index(): Response
+    {
+        return $this->render('page/index.html.twig', []);
+    }
+
+    #[Route('/about', name: 'about')]
+    public function about(): Response
+    {
+        return $this->render('page/about.html.twig', []);
+    }
+
+    #[Route('/contact', name: 'contact')]
+    public function contact(ManagerRegistry $doctrine, Request $request): Response
+    {
+    $contact = new Contact();
+    $form = $this->createForm(ContactFormType::class, $contact);
+    $form->handleRequest($request);
+    if ($form->isSubmitted() && $form->isValid()) {
+        $contacto = $form->getData();    
+        $entityManager = $doctrine->getManager();    
+        $entityManager->persist($contacto);
+        $entityManager->flush();
+        return $this->redirectToRoute('index', []);
+    }
+        return $this->render('page/contact.html.twig', array(
+            'form' => $form->createView()    
+        ));
+    }
+
+    #[Route('/news', name: 'news')]
+    public function news(): Response
+    {
+        return $this->render('page/news.html.twig', []);
+    }
+
+}
