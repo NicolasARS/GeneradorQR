ae8d08f131eb65faf76bab2097786ccedd44ea3d
NicolasARS <aluriv9412@ieselcaminas.org>
2023-11-18 00:29:48 +0100

    Proyecto evaluacion 1 - JS

--- public/assets/js/registerFormValidation.js
+++ public/assets/js/registerFormValidation.js
@@ -1,36 +1,40 @@
 document.getElementById("formularioId").onsubmit = function(event) {
     var valido = true;
+    var mensajeError = '';
 
     // Validación del nombre
     var nombre = document.getElementById("registration_form_name").value;
     if (nombre.length === 0) {
-        alert("Por favor, ingresa tu nombre completo.");
+        mensajeError +="Por favor, ingresa tu nombre completo.\n";
+
         valido = false;
     }
 
     // Validación del correo electrónico
     var correo = document.getElementById("registration_form_email").value;
     if (correo.length === 0 || !correo.includes("@")) {
-        alert("Por favor, ingresa un correo electrónico válido.");
+        mensajeError +="Por favor, ingresa un correo electrónico válido.\n";
         valido = false;
     }
 
     // Validación de la contraseña
     var contraseña = document.getElementById("registration_form_plainPassword").value;
     if (contraseña.length < 8) {
-        alert("La contraseña debe tener al menos 8 caracteres.");
+        mensajeError +="La contraseña debe tener al menos 8 caracteres.\n";
         valido = false;
     }
 
     // Validación de los términos y condiciones
     var terminos = document.getElementById("registration_form_agreeTerms").checked;
     if (!terminos) {
-        alert("Debes aceptar los términos y condiciones.");
+        mensajeError +="Debes aceptar los términos y condiciones.\n";
         valido = false;
     }
 
     // Si alguna validación falla, previene el envío del formulario
     if (!valido) {
+        alert(mensajeError);
         event.preventDefault();
     }
+    return valido;
 };
--- public/assets/js/registerModal.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    setTimeout(function() {
-        document.getElementById('registroModal').style.display = 'block';
-    }, 10000); // 10000 milisegundos = 10 segundos
-});
-
-function cerrarModal() {
-    document.getElementById('registroModal').style.display = 'none';
-}
